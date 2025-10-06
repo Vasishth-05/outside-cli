af8fb08d6a4578c2e5e2ad622ed72d04bea05d92
Vasishth <learnvasishth@gmail.com>
2025-10-06 18:57:15 +0530

    torrent fixes

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+'use strict';
 
-  console.log('Welcome to the outside!')
+import fs from "fs";
+import bencode from "bencode";
+
+import dgram from "dgram"
+import { Buffer } from "buffer";
+import { parse as urlParse } from "url";
+
+const torrent = bencode.decode(fs.readFileSync('anim.jpg.torrent'));
+
+console.log(torrent.announce.toString('utf-8'));
+
+const url = urlParse(torrent.announce.toString('utf8'));
+
+// 3
+const socket = dgram.createSocket('udp4');
+// 4
+const myMsg = Buffer.from('hello?', 'utf8');
+
+const port = url.port ? Number(url.port) : 80;
+const host = url.hostname;
+
+// 5
+socket.send(myMsg, 0, myMsg.length, port, host, () => {console.log("Message sent");
+});
+// 6
+socket.on('message', msg => {
+  console.log('message is', msg);
+});
 
